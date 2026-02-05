844db6953a1ff74f14c5d27767bbe01fd37031b0
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2026-02-05 12:58:25 +0100

    add Home page and setup React entry point - Home.tsx: welcome message - index.tsx: React app entry point - tailwind.config.js: Tailwind CSS setup

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// const Home = () => {
+//   return (
+//     <>
+//       <div className='bg-[#424549] min-h-screen flex items-center justify-center'>
+//         <h1 className='text-4xl text-white font-bold'>Welcome to the Home Page</h1>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Home
