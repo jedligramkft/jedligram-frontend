9c5ab97d561975ca1accbb3fa37d222e67dc2c78
GerencserOliver <gerencser.oliver@students.jedlik.eu>
2026-02-21 16:48:01 +0100

    add background gradients to AuthLayout, Community, and AllCommunities

--- src/Layouts/AuthLayout.tsx
+++ src/Layouts/AuthLayout.tsx
@@ -4,6 +4,9 @@ import Logo from '/Images/jedligram_logo.png'
 const AuthLayout = () => {
 	return (
 		<main className='min-h-screen bg-linear-to-b from-[#35383d] via-[#2b2f34] to-[#1f2226] poppins-regular'>
+			<div className='absolute inset-0 bg-[radial-gradient(circle_at_top,rgba(59,130,246,0.18),transparent_55%)]' />
+      		<div className='absolute inset-0 bg-[radial-gradient(circle_at_80%_20%,rgba(236,72,153,0.16),transparent_40%)]' />
+      		<div className='absolute inset-0 bg-black/30' />
 			<div className='mx-auto flex w-full max-w-6xl flex-col items-center gap-8 px-4 py-16 md:px-12'>
 				<div className='mx-auto flex w-full max-w-md items-center px-4'>
 					<img src={Logo} alt='Jedligram Logo' className='mx-auto mb-6 w-20 opacity-90 brightness-200' />
