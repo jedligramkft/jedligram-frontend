6ffdc274693c6e23a2a82d6dc98d6e5f244be19e
Geher-Marcell <darkiex03@gmail.com>
2026-03-24 16:40:24 +0100

    feat: Implement EditProfile component for user profile editing

---
+++ src/Pages/Profile/EditProfile.tsx
@@ -0,0 +1,167 @@
+import { useState } from "react";
+import type { UserData } from "../../Interfaces/UserData";
+import { ProfilePictureUpload, UpdateUserProfile } from "../../api/users";
+import { InputComponent } from "../../Components/InputFields/InputComponent";
+import { TextAreaComponent } from "../../Components/InputFields/TextAreaComponent";
+import { DragnDrop } from "../../Components/DragnDrop/DragnDrop";
+import DynamicFAIcon from "../../Components/Utils/DynamicFaIcon";
+
+export const EditProfile = (props: {
+	targetUser: UserData;
+	saveCallback: (updatedUser: UserData) => void | Promise<void>;
+}) => {
+	const [editedUser, setEditedUser] = useState<UserData | null>(
+		props.targetUser,
+	);
+	const [isSavingChanges, setIsSavingChanges] = useState(false);
+	const [fileToUpload, setFileToUpload] = useState<File | null>(null);
+
+	async function handleSave(): Promise<void> {
+		if (!editedUser) return;
+		if (!props.targetUser) return;
+
+		setIsSavingChanges(true);
+		//profil adatainak frissítése
+		try {
+			const response = await UpdateUserProfile(editedUser);
+			if (response.status === 200) {
+				console.log("Profil sikeresen frissítve:", response.data);
+			} else {
+				console.warn("Nem sikerült frissíteni a profilt:", response);
+			}
+		} catch (error) {
+			console.error("Hiba történt a profil frissítésekor:", error);
+		}
+
+		//fájl feltöltés
+		if (fileToUpload) {
+			try {
+				const response = await ProfilePictureUpload(fileToUpload);
+				setEditedUser({
+					...editedUser,
+					image_url: response.data.imageUrl,
+				});
+			} catch (err) {
+				const message =
+					err instanceof Error
+						? err.message
+						: "Nem sikerült feltölteni a profilképet.";
+				alert(`Hiba: ${message}`);
+			}
+			setFileToUpload(null);
+		}
+
+		await props.saveCallback(editedUser);
+		setIsSavingChanges(false);
+	}
+
+	const onProfilePictureSelected = async (file: File) => {
+		if (!file) return;
+		if (!props.targetUser) return;
+
+		await setFileToUpload(file);
+	};
+
+	return (
+		<>
+			<div className="p-8 border-t border-gray-700/50">
+				<h2 className="text-2xl font-bold mb-6">Fiók beállítások</h2>
+				<div className="grid md:grid-cols-2 gap-6">
+					<div>
+						<InputComponent
+							label="Felhasználónév"
+							placeholder="jedlik_user"
+							value={editedUser?.name ?? ""}
+							onChange={(e) =>
+								setEditedUser({
+									...editedUser,
+									name: e.target.value,
+								} as UserData)
+							}
+						/>
+					</div>
+					<div>
+						<InputComponent
+							label="Email"
+							placeholder="email@pelda.hu"
+							value={editedUser?.email ?? ""}
+							onChange={(e) =>
+								setEditedUser({
+									...editedUser,
+									email: e.target.value,
+								} as UserData)
+							}
+							type={"email"}
+						/>
+					</div>
+					<div className="md:col-span-2">
+						<TextAreaComponent
+							label="Bemutatkozás"
+							placeholder="Pár szó magadról..."
+							value={editedUser?.bio ?? ""}
+							onChange={(e) => {
+								setEditedUser({
+									...editedUser,
+									bio: e.target.value,
+								} as UserData);
+							}}
+							rows={3}
+							textAreaClassName="resize-none"
+						/>
+					</div>
+					<div className="md:col-span-2">
+						{(fileToUpload && (
+							<div className="mb-4 p-4 bg-green-600/20 border border-green-600 rounded">
+								<p className="text-sm text-green-300">
+									<p>Fájl kiválasztva: </p>
+									<img
+										src={URL.createObjectURL(fileToUpload)}
+										alt="Preview"
+										className="h-10 w-10 object-cover rounded-full inline-block ml-2"
+									/>
+									<button
+										className="p-4"
+										onClick={() => setFileToUpload(null)}
+									>
+										<DynamicFAIcon exportName="faX" />
+									</button>
+								</p>
+							</div>
+						)) || (
+							<DragnDrop
+								onFileSelected={onProfilePictureSelected}
+								title="Profilkép feltöltése"
+								description="Húzd ide az új profilképet, vagy kattints ide a kiválasztáshoz."
+							/>
+						)}
+						<p className="text-xs text-gray-500 mt-1">
+							Utoljára mentve:{" "}
+							{/* {formatDateTime(lastSavedAt)} */}
+						</p>
+					</div>
+				</div>
+				<div className="mt-8 flex flex-col md:flex-row items-center justify-between gap-4">
+					<button
+						onClick={handleSave}
+						disabled={isSavingChanges}
+						className="w-full md:w-auto flex items-center justify-center gap-2 rounded-lg bg-blue-600 px-6 py-3 text-sm font-semibold text-white shadow-lg transition hover:bg-blue-700 disabled:bg-gray-500 disabled:cursor-wait"
+					>
+						{isSavingChanges ? (
+							<DynamicFAIcon
+								exportName="faSpinner"
+								className="animate-spin"
+							/>
+						) : (
+							<DynamicFAIcon exportName="faSave" />
+						)}
+						<span>
+							{isSavingChanges
+								? "Mentés..."
+								: "Változtatások mentése"}
+						</span>
+					</button>
+				</div>
+			</div>
+		</>
+	);
+};
--- src/Pages/Profile/UserProfile.tsx
+++ src/Pages/Profile/UserProfile.tsx
@@ -2,16 +2,10 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import type { UserData } from "../../Interfaces/UserData";
-import {
-	GetUserProfile,
-	ProfilePictureUpload,
-	UpdateUserProfile,
-} from "../../api/users";
+import { GetUserProfile } from "../../api/users";
 import DynamicFAIcon from "../../Components/Utils/DynamicFaIcon";
-import { TextAreaComponent } from "../../Components/InputFields/TextAreaComponent";
-import { InputComponent } from "../../Components/InputFields/InputComponent";
 import { Logout } from "../../api/auth";
-import { DragnDrop } from "../../Components/DragnDrop/DragnDrop";
+import { EditProfile } from "./EditProfile";
 
 const profileStorageKey =
 	import.meta.env.VITE_PROFILE_STORAGE_KEY ?? "jedligram_profile";
@@ -41,11 +35,6 @@ const UserProfile = () => {
 	const [isMyProfile, setIsMyProfile] = useState(false);
 	const [targetUser, setTargetUser] = useState<UserData | null>(null);
 
-	const [editedUser, setEditedUser] = useState<UserData | null>(null);
-	const [isSavingChanges, setIsSavingChanges] = useState(false);
-
-	const [fileToUpload, setFileToUpload] = useState<File | null>(null);
-
 	const checkIsMyProfile = async () => {
 		const requesterId = JSON.parse(
 			localStorage.getItem(profileStorageKey) || "{}",
@@ -73,7 +62,6 @@ const UserProfile = () => {
 				}
 				//Successfully got user data
 				setTargetUser(response.data);
-				setEditedUser(response.data);
 			} catch (err) {
 				console.error(
 					"Hiba történt a felhasználói adatok lekérése közben:",
@@ -87,13 +75,6 @@ const UserProfile = () => {
 		getViewedUserProfile(Number(params.id));
 	}, []);
 
-	const onProfilePictureSelected = async (file: File) => {
-		if (!file) return;
-		if (!targetUser) return;
-
-		await setFileToUpload(file);
-	};
-
 	const handleLogout = async () => {
 		try {
 			await Logout();
@@ -106,44 +87,6 @@ const UserProfile = () => {
 			navigate("/auth/login", { replace: true });
 		}
 	};
-
-	async function handleSave(): Promise<void> {
-		if (!editedUser) return;
-		if (!targetUser) return;
-
-		setIsSavingChanges(true);
-		//profil adatainak frissítése
-		try {
-			const response = await UpdateUserProfile(editedUser);
-			if (response.status === 200) {
-				setTargetUser(response.data);
-				console.log("Profil sikeresen frissítve:", response.data);
-			} else {
-				console.warn("Nem sikerült frissíteni a profilt:", response);
-			}
-		} catch (error) {
-			console.error("Hiba történt a profil frissítésekor:", error);
-		}
-
-		//fájl feltöltés
-		if (fileToUpload) {
-			try {
-				const response = await ProfilePictureUpload(fileToUpload);
-				setTargetUser({
-					...targetUser,
-					image_url: response.data.user.image_url,
-				});
-			} catch (err) {
-				const message =
-					err instanceof Error
-						? err.message
-						: "Nem sikerült feltölteni a profilképet.";
-				alert(`Hiba: ${message}`);
-			}
-			setFileToUpload(null);
-		}
-		setIsSavingChanges(false);
-	}
 
 	return (
 		<>
@@ -218,123 +161,25 @@ const UserProfile = () => {
 								</p>
 							</div>
 						</div>
-						<div className="p-8 border-t border-gray-700/50">
-							<h2 className="text-2xl font-bold mb-6">
-								Fiók beállítások
-							</h2>
-							<div className="grid md:grid-cols-2 gap-6">
-								<div>
-									<InputComponent
-										label="Felhasználónév"
-										placeholder="jedlik_user"
-										value={editedUser?.name ?? ""}
-										onChange={(e) =>
-											setEditedUser({
-												...editedUser,
-												name: e.target.value,
-											} as UserData)
-										}
-									/>
-								</div>
-								<div>
-									<InputComponent
-										label="Email"
-										placeholder="email@pelda.hu"
-										value={editedUser?.email ?? ""}
-										onChange={(e) =>
-											setEditedUser({
-												...editedUser,
-												email: e.target.value,
-											} as UserData)
-										}
-										type={"email"}
-									/>
-								</div>
-								<div className="md:col-span-2">
-									<TextAreaComponent
-										label="Bemutatkozás"
-										placeholder="Pár szó magadról..."
-										value={editedUser?.bio ?? ""}
-										onChange={(e) => {
-											setEditedUser({
-												...editedUser,
-												bio: e.target.value,
-											} as UserData);
-										}}
-										rows={3}
-										textAreaClassName="resize-none"
-									/>
-								</div>
-								<div className="md:col-span-2">
-									{(fileToUpload && (
-										<div className="mb-4 p-4 bg-green-600/20 border border-green-600 rounded">
-											<p className="text-sm text-green-300">
-												<p>Fájl kiválasztva: </p>
-												<img
-													src={URL.createObjectURL(
-														fileToUpload,
-													)}
-													alt="Preview"
-													className="h-10 w-10 object-cover rounded-full inline-block ml-2"
-												/>
-												<button
-													className="p-4"
-													onClick={() =>
-														setFileToUpload(null)
-													}
-												>
-													<DynamicFAIcon exportName="faX" />
-												</button>
-											</p>
-										</div>
-									)) || (
-										<DragnDrop
-											onFileSelected={
-												onProfilePictureSelected
-											}
-											title="Profilkép feltöltése"
-											description="Húzd ide az új profilképet, vagy kattints ide a kiválasztáshoz."
-										/>
-									)}
-									<p className="text-xs text-gray-500 mt-1">
-										Utoljára mentve:{" "}
-										{/* {formatDateTime(lastSavedAt)} */}
-									</p>
-								</div>
-							</div>
-							<div className="mt-8 flex flex-col md:flex-row items-center justify-between gap-4">
-								<button
-									onClick={handleSave}
-									disabled={isSavingChanges}
-									className="w-full md:w-auto flex items-center justify-center gap-2 rounded-lg bg-blue-600 px-6 py-3 text-sm font-semibold text-white shadow-lg transition hover:bg-blue-700 disabled:bg-gray-500 disabled:cursor-wait"
-								>
-									{isSavingChanges ? (
-										<DynamicFAIcon
-											exportName="faSpinner"
-											className="animate-spin"
-										/>
-									) : (
-										<DynamicFAIcon exportName="faSave" />
-									)}
-									<span>
-										{isSavingChanges
-											? "Mentés..."
-											: "Változtatások mentése"}
-									</span>
-								</button>
-							</div>
-						</div>
 
-						{/* Logout */}
-						<div className="p-6 border-t border-gray-700/50 text-center">
-							<button
-								onClick={handleLogout}
-								className="flex items-center justify-center w-full md:w-auto md:mx-auto gap-2 text-sm font-semibold text-red-500 transition hover:text-red-400"
-							>
-								<DynamicFAIcon exportName="faSignOutAlt" />
-								Kijelentkezés
-							</button>
-						</div>
+						{isMyProfile && targetUser && (
+							<>
+								<EditProfile
+									targetUser={targetUser}
+									saveCallback={setTargetUser}
+								/>
+								{/* Logout */}
+								<div className="p-6 border-t border-gray-700/50 text-center">
+									<button
+										onClick={handleLogout}
+										className="flex items-center justify-center w-full md:w-auto md:mx-auto gap-2 text-sm font-semibold text-red-500 transition hover:text-red-400"
+									>
+										<DynamicFAIcon exportName="faSignOutAlt" />
+										Kijelentkezés
+									</button>
+								</div>
+							</>
+						)}
 					</div>
 				</div>
 			</section>
