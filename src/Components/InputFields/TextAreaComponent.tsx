54d0316ec5689a5bbcea7da42296b3e1efdc81c3
Geher-Marcell <darkiex03@gmail.com>
2026-04-01 16:51:10 +0200

    fix(TextAreaComponent): streamline data-1p-ignore attribute formatting

--- src/Components/InputFields/TextAreaComponent.tsx
+++ src/Components/InputFields/TextAreaComponent.tsx
@@ -51,9 +51,7 @@ export const TextAreaComponent = (props: {
 					className={`w-full rounded-xl border border-white/10 bg-white/10 px-4 py-3 text-white placeholder:text-white/50 outline-none focus:border-white/20 ${props.textAreaClassName || ""}`}
 					data-bwignore={ignorePasswordManager ? "true" : undefined}
 					data-lpignore={ignorePasswordManager ? "true" : undefined}
-					data-1p-ignore={
-						ignorePasswordManager ? "true" : undefined
-					}
+					data-1p-ignore={ignorePasswordManager ? "true" : undefined}
 				/>
 				{absoluteChildren}
 			</div>
